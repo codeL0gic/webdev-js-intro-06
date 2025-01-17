fac2f36046a7dbc60e57e8a73331f4af2223883d
codeL0gic <shannon.neumann@yum.com>
2025-01-16 19:22:10 -0500

    main

--- js/assignment.js
+++ js/assignment.js
@@ -6,34 +6,43 @@ const sumTheNumbersElement = document.getElementById("sum-the-numbers");
 const createNumberArrayElement = document.getElementById("create-number-array");
 
 function evenOrOdd() {
-    const num = 3;
-    // Write the logic to decide if the variable "num" is even or odd
-    // and set the element's value the string "Even" or "Odd" accordingly
-
+  const num = 3;
+  // Write the logic to decide if the variable "num" is even or odd
+  // and set the element's value the string "Even" or "Odd" accordingly
+  evenOrOddElement.innerHTML = "Even";
+  evenOrOddElement.innerHTML = "Odd";
 }
 
 function sumTheNumbers() {
-    let sum = 0;
-    // Write the logic to sum the numbers 1 through 10
-    // using a for loop. Check the expected output
-    // on the assignment page
+  let sum;
+  sumTheNumbersElement.innerHTML = sum;
+  // Write the logic to sum the numbers 1 through 10
+  // using a for loop. Check the expected output
+  // on the assignment page
 
+  for (let i = 1; i <= 10; i++) {
+    // if (sum += i[(1, 3, 6, 10, 15, 21, 28, 36, 45, 55)]);
+    sumTheNumbersElement.innerHTML = sum;
+  }
 }
 
 function createNumberArray() {
-    const numberArray = [];
-
-    // Write the logic that loops 10 times and adds the value
-    // to numberArray each iteration. Check the expected output
-    // on the assignment page
-
+  numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  createNumberArrayElement.innerHTML = numberArray;
+  // Write the logic to create an array of numbers 1 through 10
+  while ((numberArray.length < 10, ++numberArray)) {
+    numberArray.push();
+  }
 }
-
+// to numberArray each iteration. Check the expected output
+// on the assignment page
 function render() {
-    // Call the created functions
-
+  // Call the created functions
+  evenOrOdd();
+  sumTheNumbers();
+  createNumberArray();
 }
 
 submissionBtn.addEventListener("click", function () {
-    render();
+  render();
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,65 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+/*
+for (let i = 0; i < 8; i++) {
+  console.log(i);
+}
+*/
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+//   continue;
+// }
+
+// for (let i = 0; i < 8; i++) {
+// console.log(i);
+// }
+
+// let i = 0;
+// while (i < 8) {
+//   console.log(i);
+//   i++;
+// }
+
+//
+
+/* Break and Continue */
+// Break  - This statement is used to exit a loop early or switch the statement & transfers program control to the statement immediately following the loop.  It can also be used to jump past a labeled statement when used within that labeled statement.
+// let i = 0;
+
+// while (i < 6) {
+//   if (i === 3) {
+//     break;
+//   }
+//   i = i + 1;
+// }
+
+// console.log(i);
+// // Expected output: 3
+
+// // Continue - This statement is used to skip the current iteration of a loop and continue with the next iteration.  It can also be used to jump to the next iteration of a labeled statement.
+// let text = "";
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 3) {
+//     continue;
+//   }
+//   text = text + i;
+// }
+
+// console.log(text);
+// // Expected output: "012456789"
